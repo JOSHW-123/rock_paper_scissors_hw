92b9e773c1bdcf1299c7ffd25ada01b0a470f07c
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-02 17:31:28 +0100

    Playe class created

---
+++ models/player.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__(self, decision, name):
+        self.decision = decision
+        self.name = name 
+
+
---
+++ models/rock_paper_scissors.py
@@ -0,0 +1,2 @@
+class RockPaperScissors:
+    def __init__(self, user1=None, user2=None):
