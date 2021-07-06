80f620f66dabf41bc29383aefd5229f81fb94f6b
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-06 10:11:13 +0100

    .

--- models/game.py
+++ models/game.py
@@ -1,17 +1,33 @@
 class Game:
-    def __init__(self, player_1, player_2):
-        self.player_1 = player_1
-        self.player_2 = player_2
+    def __init__(self):
+        self.win_dict = {
+            "scissors" : "paper",
+            "paper" : "rock",
+            "rock" : "scissors"
+        }
 
-    def draw(self, player_1, player_2):
-        if player_1.choice == player_2.choice:
-            return "It's a draw, better luck next time"
+    def play(self, player_1, player_2):
+    
+        winner = None
+
+        if self.win_lookup[player_1.choice.lower()] == player_2.choice.lower():
+            winner = player_1
+        elif self.win_lookup[player_2.choice.lower()] == player_1.choice.lower():
+            winner = player_2
+
+        return winner
+   
         
-    def winner(self, player_1, player_2):
-        if (player_1.choice == "Rock" and player_2.choice == "Scissors") or (player_1.choice == "Paper" and player_2.choice == "Rock") or (player_1.choice == "Scissors" and player_2.choice == "Paper"):
-            return player_1.name + ("Wins")
-        else:
-            return player_2.name + ("Wins")
+
+    # def draw(self, player_1, player_2):
+    #     if player_1.choice == player_2.choice:
+    #         return "It's a draw, better luck next time"
+        
+    # def winner(self, player_1, player_2):
+    #     if (player_1.choice == "Rock" and player_2.choice == "Scissors") or (player_1.choice == "Paper" and player_2.choice == "Rock") or (player_1.choice == "Scissors" and player_2.choice == "Paper"):
+    #         return player_1.name + ("Wins")
+    #     else:
+    #         return player_2.name + ("Wins")
 
 
 
